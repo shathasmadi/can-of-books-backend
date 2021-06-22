dd03481fffe9df055fbf6ece5ac0871f123af7d2
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-06-22 16:55:37 +0300

    add delete and create backend

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -1,15 +1,45 @@
 "use strict";
 
-const userModel = require("../models/user.model");
+const { userModel } = require("../models/user.model");
 
 const getBook = (req, res) => {
-    const { email } = req.query;
-    userModel.find({ email: email }, (error, user) => {
-        if (error) {
-            res.send(error);
-        } else {
-            res.json(user);
-        }
-    });
+  const { email } = req.query;
+  userModel.findOne({ email: email }, (error, user) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(user);
+    }
+  });
 };
-module.exports = getBook;
+
+const createBook = (req, res) => {
+  const { email, bookName, bookDescription, bookStatus } = req.body;
+
+  userModel.findOne({ email: email }, (error, userData) => {
+    if (error) {
+      res.send(error);
+    } else {
+      userData.books.push({ name: bookName, description: bookDescription, status: bookStatus });
+      userData.save();
+      res.json(userData);
+    }
+  });
+};
+
+const deleteBook = (req, res) => {
+  const bookIndex = req.params.book_idx;
+  const { email } = req.query;
+
+  userModel.findOne({ email: email }, (error, userData) => {
+    if (error) {
+      res.send(error);
+    } else {
+      userData.books.splice(bookIndex, 1);
+      userData.save();
+      res.json(userData);
+    }
+  });
+};
+
+module.exports = { getBook, createBook, deleteBook };
--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const getBook = require("./controller/book.controller");
+const { getBook, createBook, deleteBook } = require("./controller/book.controller");
 
 const indexController = require("./controller/index.controller");
 
 require("dotenv").config();
 const PORT = process.env.PORT;
 
-const seedUserData = require("./models/user.model");
+const { seedUserData } = require("./models/user.model");
 const axios = require("axios");
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/myFavoriteBooks", { useNewUrlParser: true, useUnifiedTopology: true });
 
-// seedUserData();
+seedUserData();
 
 app.get("/", indexController);
 app.get("/books", getBook);
+app.post("/book", createBook);
+app.delete("/book/:book_idx", deleteBook);
 
 app.listen(PORT, () => console.log(`listening ${PORT}`));
