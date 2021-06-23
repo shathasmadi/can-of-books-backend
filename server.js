b99771b3f6007023282b7c57daad11b4481f8111
root <root@DESKTOP-H8Q20A4.localdomain>
2021-06-23 17:10:02 +0300

    edit

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const { getBook, createBook, deleteBook, updateBook } = require("./controller/book.controller");
+const { getBook, createBook, deleteBook } = require("./controller/book.controller");
 
 const indexController = require("./controller/index.controller");
 
 require("dotenv").config();
 const PORT = process.env.PORT;
-// const MONGO_URL = process.env.MONGO_URL;
 
 const { seedUserData } = require("./models/user.model");
 const axios = require("axios");
@@ -16,7 +15,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`${process.env.MONGO_URL}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // seedUserData();
 
@@ -24,6 +23,5 @@ app.get("/", indexController);
 app.get("/books", getBook);
 app.post("/book", createBook);
 app.delete("/book/:id", deleteBook);
-app.put("/book/:book_idx", updateBook);
 
 app.listen(PORT, () => console.log(`listening ${PORT}`));
